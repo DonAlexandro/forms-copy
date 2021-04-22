ce5c406f5614fd79f222302b6d20f4cfe5bc0d16
Olexandr <olexandr.pylypyshyn@nordwhale.com>
2021-04-23 00:24:24 +0300

    add configured for development client folder

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Typography} from 'antd'
+
+const {Title} = Typography
+
+function App() {
+	return (
+		<div>
+			<Title>Hello World</Title>
+		</div>	
+	)
+}
+
+export default App
